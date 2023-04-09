5b71e0ba059221a4eb8b05e16c19203009248cf4
CadeLui <clui83@outlook.com>
2023-04-09 16:19:31 -0400

    MORE ATTEMPTS

--- compound_smooth_linear.cpp
+++ compound_smooth_linear.cpp
@@ -10,24 +10,34 @@
 
 */
 
-double leftMotorMod(int joyX, double amp, int joyY)
+double leftMotorMod(int joyX, int joyY)
 {
-    double theta = acos(joyX/amp);
-    if (joyY < 0) theta += M_PI;
-    if (theta > M_PI/2 and theta < M_PI*3/2)
-        return (-2/M_PI)*theta +2;
+    double theta = atan2(joyY, joyX);
+    if (joyY < 0) theta += (2*M_PI);
+    if (theta >= 0 && theta < M_PI/2)
+        return -2*theta/M_PI+2;
+    if (theta >= M_PI/2 && theta < M_PI)
+        return -6*theta/M_PI+4;
+    if (theta >= M_PI && theta < 3*M_PI/2)
+        return 2*theta/M_PI-4;
+    if (theta >= 3*M_PI/2)
+        return 6*theta/M_PI-10;
     else
         return 1;
 }
 
-double rightMotorMod(int joyX, double amp, int joyY)
+double rightMotorMod(int joyX, int joyY)
 {
-    double theta = acos(joyX/amp);
-    if (joyY < 0) theta += M_PI;
-    if (theta < M_PI/2)
-        return (2/M_PI)*theta;
-    else if (theta > 3*M_PI/2)
-        return (2/M_PI)*theta - 4;
+    double theta = atan2(joyY, joyX);
+    if (joyY < 0) theta += (2*M_PI);
+    if (theta >= 0 && theta < M_PI/2)
+        return 6*theta/M_PI-2;
+    if (theta >= M_PI/2 && theta < M_PI)
+        return 2*theta/M_PI;
+    if (theta >= M_PI && theta < 3*M_PI/2)
+        return -6*theta/M_PI+8;
+    if (theta >= 3*M_PI/2)
+        return -2*theta/M_PI+2;
     else
         return 1;
 }
@@ -44,9 +54,8 @@ std::vector<int> motorSpeeds(int joyY, int joyX)
             value defined by the horizontal position of the joystick.
         Value 0 is the speed of the left motor, Value 1 is the speed of the right motor.
     */
-    int moveVector = sqrt(joyY*joyY + joyX*joyX);
-    if (joyY < 0) moveVector *= -1;
-    std::vector<int> speeds {int(moveVector*leftMotorMod(joyX, moveVector, joyY)), int(moveVector*rightMotorMod(joyX, moveVector, joyY))};
+    int moveVector = sqrt(joyY*joyY + joyX*joyX)/2;
+    std::vector<int> speeds {int(moveVector*leftMotorMod(joyX, joyY)), int(moveVector*rightMotorMod(joyX, joyY))};
     return speeds;
 }
 
@@ -60,14 +69,14 @@ std::vector<int> motorSpeeds(int joyY, int joyX)
 std::vector<int> genJoyY(int iterator=15, int amp=100)
 {
     std::vector<int> values;
-    for (double i = 0; i < 360; i += iterator)
+    for (double i = 0; i < 361; i += iterator)
         values.push_back( (int) (amp*sin((i*M_PI)/180)) );
     return values;
 }
 std::vector<int> genJoyX(int iterator=15, int amp=100)
 {
     std::vector<int> values;
-    for (double i = 0; i < 360; i += iterator)
+    for (double i = 0; i < 361; i += iterator)
         values.push_back( (int) (amp*cos((i*M_PI)/180)) );
     return values;
 }
@@ -84,11 +93,11 @@ int main(void)
         std::vector<int> motors = motorSpeeds(yVals.at(i), xVals.at(i));
         std::cout << yVals.at(i) << ":" << xVals.at(i) << " | " 
         << motors.at(0) << ":" << motors.at(1) << " | " 
-        << i*iterator <<  "\n";
+        << i*iterator <<  " | "
+        << leftMotorMod(xVals.at(i), yVals.at(i)) << " | " 
+        << rightMotorMod(xVals.at(i), yVals.at(i)) << " | "
+        <<"\n";
     }
-    std::cout << "\n";
-    //for (int x = -100; x < 101; x += 10)
-    //    std::cout << leftMotorMod(x) << " | ";
     std::cout << "\n";
     return 0;
 }
